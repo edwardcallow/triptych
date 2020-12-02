033a26d8dea37d1ad425e2a1d247d360484c1f39
Edward Callow <programming_development@edwardcallow.co.uk>
2020-12-02 01:19:46 +0000

    Test session for 1:15

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 // On page load
 window.onload = function () {
 
-    var testPeriodStart     = new Date(Date.UTC(2020, 11, 2, 1, 5, 0));
-    var testPeriodEnd       = new Date(Date.UTC(2020, 11, 2, 1, 30, 0));
+    var testPeriodStart     = new Date(Date.UTC(2020, 11, 2, 13, 15, 0));
+    var testPeriodEnd       = new Date(Date.UTC(2020, 11, 2, 13, 20, 0));
 
     var mondayShowStart     = new Date(Date.UTC(2020, 11, 1, 18, 50));
     var mondayShowEnd       = new Date(Date.UTC(2020, 11, 1, 20, 20));
